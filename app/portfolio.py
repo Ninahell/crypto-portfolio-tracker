649a44c322e4c3662faf7103cdd3e57950de2769
Nina <geleishaelena@yandex.ru>
2026-05-07 11:38:48 +0300

    refactor: migrate portfolio persistence to repository pattern

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,28 +1,31 @@
 from collections import defaultdict
 from typing import List
-from app.transaction import Transaction
-from app.storage import Storage
+
 from app.logger import logger
+from app.repositories.transaction_repository import TransactionRepository
+from app.transaction import Transaction
+
 
 class Portfolio:
     def __init__(self):
-        self.storage = Storage()
-        self.transactions: List[Transaction] = self.storage.load()
+        self.repository = TransactionRepository()
+        self.transactions: List[Transaction] = self.repository.get_all()
 
     def add_transaction(self, tx: Transaction):
+        self.repository.add(tx)
         self.transactions.append(tx)
-self.storage.save(self.transactions)
 
-logger.info(
-    f"Transaction added: {tx.asset} | amount={tx.amount} | price={tx.price}"
-)
+        logger.info(
+            f"Transaction added: {tx.asset} | amount={tx.amount}"
+        )
 
     def get_balance(self):
         balance = defaultdict(float)
+
         for tx in self.transactions:
             balance[tx.asset] += tx.amount
+
         return dict(balance)
 
     def total_value(self):
         return sum(tx.value() for tx in self.transactions)
-
