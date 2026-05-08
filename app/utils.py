446b44dd10f0d79d8e2fbad0609d480f07935cc7
Nina <geleishaelena@yandex.ru>
2026-05-08 14:23:41 +0300

    refactor: improve typing in utility functions

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,15 @@
 import csv
 
 
-def export_transactions_to_csv(transactions, filename="report.csv"):
+from typing import List
+
+from app.transaction import Transaction
+
+
+def export_transactions_to_csv(
+    transactions: List[Transaction],
+    filename: str = "report.csv"
+):
     with open(filename, "w", newline="") as file:
         writer = csv.writer(file)
 
