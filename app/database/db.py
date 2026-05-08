c428b77ee7b6151352e7229f212963f01e80e946
Nina <geleishaelena@yandex.ru>
2026-05-08 14:18:57 +0300

    refactor: move database settings to config module

--- app/database/db.py
+++ app/database/db.py
@@ -1,8 +1,9 @@
 import sqlite3
 
+from app.config import DATABASE_NAME
 
 class Database:
-    def __init__(self, db_name="portfolio.db"):
+def __init__(self, db_name=DATABASE_NAME):
         self.connection = sqlite3.connect(db_name)
         self.cursor = self.connection.cursor()
 
