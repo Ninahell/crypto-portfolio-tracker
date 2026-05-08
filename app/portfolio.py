7a36087089de6cbbafc29a9fd23a11bccf8cc095
Nina <geleishaelena@yandex.ru>
2026-05-08 14:17:10 +0300

    refactor: simplify portfolio entity model

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,31 +1,9 @@
-from collections import defaultdict
+from dataclasses import dataclass
 from typing import List
 
-from app.logger import logger
-from app.repositories.transaction_repository import TransactionRepository
 from app.transaction import Transaction
 
 
+@dataclass
 class Portfolio:
-    def __init__(self):
-        self.repository = TransactionRepository()
-        self.transactions: List[Transaction] = self.repository.get_all()
-
-    def add_transaction(self, tx: Transaction):
-        self.repository.add(tx)
-        self.transactions.append(tx)
-
-        logger.info(
-            f"Transaction added: {tx.asset} | amount={tx.amount}"
-        )
-
-    def get_balance(self):
-        balance = defaultdict(float)
-
-        for tx in self.transactions:
-            balance[tx.asset] += tx.amount
-
-        return dict(balance)
-
-    def total_value(self):
-        return sum(tx.value() for tx in self.transactions)
+    transactions: List[Transaction]
